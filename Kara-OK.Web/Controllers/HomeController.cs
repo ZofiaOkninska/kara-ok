a1de1b0b3cd233f1dc695b50e92e498dd9ef6626
Zofia Okninska <zokninska@gmail.com>
2026-01-10 09:44:57 +0100

    Personalize home and logout views

--- Kara-OK.Web/Controllers/HomeController.cs
+++ Kara-OK.Web/Controllers/HomeController.cs
@@ -1,20 +1,30 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Kara_OK.Web.Models;
+using Kara_OK.Web.Models.Identity;
 
 namespace Kara_OK.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly UserManager<ApplicationUser> _userManager;
+    
+    public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        if (User.Identity?.IsAuthenticated == true)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            ViewBag.FirstName = user?.FirstName;
+        }
+
         return View();
     }
 
